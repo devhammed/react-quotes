3b4e43499bebfbd4ec050ce78c13ab8ab8f566d4
Oyedele Hammed <itz.harmid@gmail.com>
2018-11-27 13:51:57 +0100

    Updates

---
+++ precache-manifest.1cbf89fa73daf65b532976347d985f5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1fff4324b16dbb390334b329a3b6d745",
+    "url": "/react-quotes/static/media/twitter-square.1fff4324.svg"
+  },
+  {
+    "revision": "ec01276663117ad6375905c86357804e",
+    "url": "/react-quotes/static/media/quote-left.ec012766.svg"
+  },
+  {
+    "revision": "aa968275a224dde66907",
+    "url": "/react-quotes/static/js/runtime~main.aa968275.js"
+  },
+  {
+    "revision": "122d85055703a000ed19",
+    "url": "/react-quotes/static/js/main.122d8505.chunk.js"
+  },
+  {
+    "revision": "d91b9606f4e8a16aaf51",
+    "url": "/react-quotes/static/js/1.d91b9606.chunk.js"
+  },
+  {
+    "revision": "122d85055703a000ed19",
+    "url": "/react-quotes/static/css/main.a0e68075.chunk.css"
+  },
+  {
+    "revision": "5f2f2a89a3cbeba2579c263128ecc86b",
+    "url": "/react-quotes/index.html"
+  }
+];
