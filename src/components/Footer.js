40763bc1796346caf4f38ec1b1a91321d4973ba2
Oyedele Hammed <itz.harmid@gmail.com>
2018-12-03 13:19:59 +0100

    Updated quoting!

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,8 @@ import './Footer.css'
 export default class Footer extends Component {
   render () {
     return (
-      <footer className='Footer'>
-        Made by <a href='https://devhammed.github.io' className='Footer__link'>Oyedele Hammed</a> |
+      <footer className="Footer">
+        Made by <a href="https://devhammed.github.io" className="Footer__link">Oyedele Hammed</a> |
         <strong>#LearningReact2018</strong>
       </footer>
     )
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,8 @@ import './Header.css'
 export default class Header extends Component {
   render () {
     return (
-      <header className='Header'>
-        <h1 className='Header__text'>
+      <header className="Header">
+        <h1 className="Header__text">
           React Quotes Machine
         </h1>
       </header>
